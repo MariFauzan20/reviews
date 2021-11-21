282c0ed0b4dfe920c667aaceaf88c24ed4a357dc
Mar'i Fauzan <marifauzan20@gmail.com>
2021-11-21 20:44:03 +0700

    Updates

---
+++ precache-manifest.aa75985302a2cf5db3f05d29a918d7df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91ab30f0dd7b45ab231b8f74784b360f",
+    "url": "/reviews/index.html"
+  },
+  {
+    "revision": "18214b978bd423a2ac43",
+    "url": "/reviews/static/css/main.ecb6766e.chunk.css"
+  },
+  {
+    "revision": "e1a3c50a15b44825207f",
+    "url": "/reviews/static/js/2.bdbc782b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reviews/static/js/2.bdbc782b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18214b978bd423a2ac43",
+    "url": "/reviews/static/js/main.0fc75433.chunk.js"
+  },
+  {
+    "revision": "5496dc53684f16f6acfe",
+    "url": "/reviews/static/js/runtime-main.51a67177.js"
+  }
+]);
